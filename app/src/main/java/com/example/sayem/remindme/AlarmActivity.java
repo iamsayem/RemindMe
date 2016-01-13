482b1a079f6412febc2cd93aaec851734d4f5acf
sayemBari <sayemobscure040@gmail.com>
2016-01-13 21:45:50 +0600

    Sixth Commit

--- app/src/main/java/com/example/sayem/remindme/AlarmActivity.java
+++ app/src/main/java/com/example/sayem/remindme/AlarmActivity.java
@@ -1,9 +1,9 @@
 package com.example.sayem.remindme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class AlarmActivity extends AppCompatActivity {
+public class AlarmActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
